e004b50c7843548fc7fda45a5e7fd713fe258499
Carlos <ixarlos@gmail.com>
2020-05-19 14:16:58 -0500

    Updates

---
+++ precache-manifest.a1c90eed68b54264d04642ed280e1759.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bda44bb26bf8f7723ff679f20a55d90b",
+    "url": "/crehana/index.html"
+  },
+  {
+    "revision": "4e1007736c42ec9da37a",
+    "url": "/crehana/static/css/main.e5669473.chunk.css"
+  },
+  {
+    "revision": "3e85357c903ec4ecebf1",
+    "url": "/crehana/static/js/2.2bef22f8.chunk.js"
+  },
+  {
+    "revision": "4f4a12233651b0855fd74d4d32936871",
+    "url": "/crehana/static/js/2.2bef22f8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e1007736c42ec9da37a",
+    "url": "/crehana/static/js/main.832df345.chunk.js"
+  },
+  {
+    "revision": "df612521b7b7b070e420",
+    "url": "/crehana/static/js/runtime-main.77b32e9a.js"
+  }
+]);
